ec23452ab29abe2584d06ec497eff27b8825884b
z.mouchttati@gmail.com <z.mouchttati@gmail.com>
2024-05-13 16:39:01 +0100

    Your commit message here

--- index.ts
+++ index.ts
@@ -2,14 +2,29 @@ import express, { Request, Response } from "express";
 import prisma from "./prisma/prisma";
 import { createLead, getLeads } from "./controllers/leadController";
 import cors from 'cors'
+import { authorize, writeData } from "./controllers/spreadSheets";
 const app = express();
 app.use(express.json());
 app.use(cors())
 
 // root routes
 app.get("/", async (req: Request, res: Response) => {
-  res.status(200).json({ message: "HELLO WORLD" });
+  res.status(200).json({ message: "HELLO WORLxsaasD" });
 });
+
+
+app.post("/spread-sheets" ,async (req: Request , res : Response ) => {
+
+  const lead = await prisma.lead.findFirst() 
+
+  console.log(lead)
+
+
+  authorize().then((auth ) =>  writeData(auth , lead )).catch(console.error);
+
+  res.send('HEY ')
+
+})
 
 app.get("/leads", getLeads);
 
